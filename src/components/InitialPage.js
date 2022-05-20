95c62921d202a94849ef847a873571b54caf9b35
Cecilia Salema <ceciliasiman15@gmail.com>
2022-05-20 08:20:37 -0300

    tentativa 1

--- src/components/InitialPage.js
+++ src/components/InitialPage.js
@@ -1,7 +1,17 @@
 import React from "react";
+import axios from 'axios';
 import { Link } from "react-router-dom";
 
 export default function InitialPage() {
+    const [items, setItems] = React.useState([]);
+
+	React.useEffect(() => {
+		const requisicao = axios.get("https://mock-api.driven.com.br/api/v5/cineflex/movies");
+
+		requisicao.then(resposta => {
+			setItems(resposta.data.items);console.log("sucesso!");
+		});
+	}, []);
   return (
     <div className="initialPage">
       <div className="topBar">
